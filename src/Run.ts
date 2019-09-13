2db802d4101a51ae3922f3831b4410d5e2212775
Russell Porter <contact@russellporter.com>
2019-09-13 21:37:09 +0200

    Add run convention & colors

--- src/Run.ts
+++ src/Run.ts
@@ -63,3 +63,93 @@ export enum ColorName {
   ORANGE = "orange",
   PURPLE = "purple"
 }
+
+export enum RunConvention {
+  EUROPE = "europe",
+  JAPAN = "japan",
+  NORTH_AMERICA = "north_america"
+}
+
+// When adding a new color, add a supplemental oneway icon on the map style
+const GREEN_COLOR = "hsl(125, 100%, 33%)";
+const BLUE_COLOR = "hsl(208, 100%, 33%)";
+const RED_COLOR = "hsl(359, 94%, 53%)";
+const BLACK_COLOR = "hsl(0, 0%, 0%)";
+const ORANGE_COLOR = "hsl(34, 100%, 50%)";
+const PURPLE_COLOR = "hsl(298, 87%, 43%)";
+
+export function getColorName(color: string): ColorName {
+  switch (color) {
+    case GREEN_COLOR:
+      return ColorName.GREEN;
+    case BLUE_COLOR:
+      return ColorName.BLUE;
+    case RED_COLOR:
+      return ColorName.RED;
+    case BLACK_COLOR:
+      return ColorName.BLACK;
+    case ORANGE_COLOR:
+      return ColorName.ORANGE;
+    case PURPLE_COLOR:
+      return ColorName.PURPLE;
+    default:
+      throw "missing color";
+  }
+}
+
+export function getRunColor(
+  convention: RunConvention,
+  difficulty: RunDifficulty
+): string {
+  switch (convention) {
+    case RunConvention.EUROPE:
+      switch (difficulty) {
+        case RunDifficulty.NOVICE:
+          return GREEN_COLOR;
+        case RunDifficulty.EASY:
+          return BLUE_COLOR;
+        case RunDifficulty.INTERMEDIATE:
+          return RED_COLOR;
+        case RunDifficulty.ADVANCED:
+        case RunDifficulty.EXPERT:
+          return BLACK_COLOR;
+        case RunDifficulty.FREERIDE:
+        case RunDifficulty.EXTREME:
+          return ORANGE_COLOR;
+        default:
+          return PURPLE_COLOR;
+      }
+    case RunConvention.JAPAN:
+      switch (difficulty) {
+        case RunDifficulty.NOVICE:
+        case RunDifficulty.EASY:
+          return GREEN_COLOR;
+        case RunDifficulty.INTERMEDIATE:
+          return RED_COLOR;
+        case RunDifficulty.ADVANCED:
+        case RunDifficulty.EXPERT:
+          return BLACK_COLOR;
+        case RunDifficulty.FREERIDE:
+        case RunDifficulty.EXTREME:
+          return ORANGE_COLOR;
+        default:
+          return PURPLE_COLOR;
+      }
+    case RunConvention.NORTH_AMERICA:
+      switch (difficulty) {
+        case RunDifficulty.NOVICE:
+        case RunDifficulty.EASY:
+          return GREEN_COLOR;
+        case RunDifficulty.INTERMEDIATE:
+          return BLUE_COLOR;
+        case RunDifficulty.ADVANCED:
+        case RunDifficulty.EXPERT:
+          return BLACK_COLOR;
+        case RunDifficulty.FREERIDE:
+        case RunDifficulty.EXTREME:
+          return ORANGE_COLOR;
+        default:
+          return PURPLE_COLOR;
+      }
+  }
+}
