f30cbc239a4bcbde7b619c2f4113d2a3fa01224b
Russell Porter <contact@russellporter.com>
2025-03-01 13:13:17 +0100

    Add source helpers

--- src/Source.ts
+++ src/Source.ts
@@ -1,3 +1,5 @@
+import { exhaustiveMatchingGuard } from './util/exhaustiveMatchingGuard'
+
 export enum SourceType {
   SKIMAP_ORG = 'skimap.org',
   OPENSTREETMAP = 'openstreetmap',
@@ -6,4 +8,26 @@ export enum SourceType {
 export type Source = {
   type: SourceType
   id: string
+}
+
+export function getSourceURL(source: Source): string {
+  switch (source.type) {
+    case SourceType.OPENSTREETMAP:
+      return 'https://www.openstreetmap.org/' + source.id
+    case SourceType.SKIMAP_ORG:
+      return 'https://www.skimap.org/SkiAreas/view/' + source.id
+    default:
+      return exhaustiveMatchingGuard(source.type)
+  }
+}
+
+export function getSourceName(type: SourceType): string {
+  switch (type) {
+    case SourceType.OPENSTREETMAP:
+      return 'OpenStreetMap'
+    case SourceType.SKIMAP_ORG:
+      return 'Skimap.org'
+    default:
+      return exhaustiveMatchingGuard(type)
+  }
 }
